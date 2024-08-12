ac4c20fe916837b0a7b76153e29b790caf0266ae
June <skyve21@naver.com>
2024-08-12 23:14:23 +0900

    EBS csi driver

---
+++ terraform/EKS_terraform/EKS_EBS_CSI/02-ebs-terraform-manifests/c4-01-ebs-csi-datasources.tf
@@ -0,0 +1,12 @@
+data "http" "ebs_csi_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+output "ebs_csi_iam_policy" {
+  value = data.http.ebs_csi_iam_policy.body
+}
