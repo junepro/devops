cbf6b00f25fbf883c5916625f7cc310e3d06d630
June <skyve21@naver.com>
2025-05-21 22:49:41 +0900

    2025-05-21 terraform

---
+++ project/aws_terraform/eks-demo/hpa/c4-02-metrics-server-install.tf
@@ -0,0 +1,12 @@
+01-deployment.yaml
+02-service.yaml
+03-hpa.yaml# Install Kubernetes Metrics Server using HELM
+# Resource: Helm Release 
+resource "helm_release" "metrics_server_release" {
+  name       = "${local.name}-metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace = "kube-system"   
+}
+
+
